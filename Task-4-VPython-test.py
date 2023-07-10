00351e2c00566bdebb721a836aff86269454dcdc
Eric Mehiel <emehiel@calpoly.edu>
2023-07-10 12:53:44 -0700

    task 4 template

---
+++ Task-4-VPython-test.py
@@ -0,0 +1,11 @@
+import vpython as vp
+
+# Draw the box and set the color to red
+box = vp.box()
+box.color = ...
+
+# start an infinite loop and set the rate to 20 frame per second
+# rotate the box 0.1 radians per loop iteration
+while ...:
+    vp.rate(...)
+    box.
