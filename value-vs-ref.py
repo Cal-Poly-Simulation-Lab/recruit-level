a92713f51f63c75faf0c3c889ffc2ebc5ab829fa
Eric Mehiel <emehiel@calpoly.edu>
2023-07-10 10:24:10 -0700

    updating code

--- value-vs-ref.py
+++ value-vs-ref.py
@@ -1,1 +1,17 @@
-# this code will modify the 3rd and 4th values 
+# Modify the code below to
+
+def mod_list(L):
+    #new_L = L
+    new_L = []
+    #for l in new_L:
+    for l in L:
+        new_L.append(2*l)
+
+    return new_L
+
+L = [1, 2, 3]
+print("the list is: ", L)
+
+new_L = mod_list(L)
+print('the list after calling the function is: ', L)
+print('the new list is: ', new_L)
