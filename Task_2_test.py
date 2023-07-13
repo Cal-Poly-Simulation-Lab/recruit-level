05f791ee35400dfd230740ae85f697a2bf20c51b
Eric Mehiel <emehiel@calpoly.edu>
2023-07-13 14:08:26 -0700

    fixing

--- Task_2_test.py
+++ Task_2_test.py
@@ -3,4 +3,6 @@ import Task_2_value_vs_ref
 def test_task2():
     L = [1, 2, 3]
     m = 2
-    assert Task_2_value_vs_ref.mod_list(L, m) == [2, 4, 6]
+    expected = [2, 4, 6]
+    result = Task_2_value_vs_ref.mod_list(L, m)
+    assert result == expected
